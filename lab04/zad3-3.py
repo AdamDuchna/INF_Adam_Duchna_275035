4f272f49d6b227c0e904eb93bc0e2205f9cd6fe4
AdamDuchna <95301576+AdamDuchna@users.noreply.github.com>
2022-03-30 12:51:24 +0200

    poprawione

--- lab04/zad3-3.py
+++ lab04/zad3-3.py
@@ -3,12 +3,13 @@ from pyswarms.utils.functions import single_obj as fx
 from pyswarms.utils.plotters.plotters import plot_surface
 from pyswarms.utils.plotters.formatters import Mesher
 from pyswarms.utils.plotters.formatters import Designer
-options = {'c1':0.5, 'c2':0.3, 'w':0.9}
-optimizer = ps.single.GlobalBestPSO(n_particles=10, dimensions=2, options=options)
+options = {'c1':0.7, 'c2':0.4, 'w':0.9}
+optimizer = ps.single.GlobalBestPSO(n_particles=40, dimensions=2, options=options)
+cost_history, pos_history = optimizer.optimize(fx.ackley, iters=100)
 # historia kosztów i pozycji
-m = Mesher(func=fx.sphere)
+m = Mesher(func=fx.ackley)
 pos_history_3d = m.compute_history_3d(optimizer.pos_history)
-d = Designer(limits=[(-1,1), (-1,1), (-0.1,1)], label=['x-axis', 'y-axis', 'z-axis'])
+d = Designer(limits=[(-1,1), (-1,4), (-0.1,6)], label=['x-axis', 'y-axis', 'z-axis'])
 #tworzenie animacji
 animation = plot_surface(pos_history=pos_history_3d,
                          mesher=m, designer=d,
